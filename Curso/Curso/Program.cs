ae4d97d55e41d7c376a489ce0193b70096a2f00d
Erick <soto.ruano.erick@gmail.com>
2021-01-13 00:08:41 -0600

    new function exercise and test proyect added

--- Curso/Curso/Program.cs
+++ Curso/Curso/Program.cs
@@ -7,9 +7,17 @@ namespace Curso
     {
         static void Main(string[] args)
         {
-            int[] exampleTest1 = { 2, 6, 8, -10, 3 };
-            Find(exampleTest1);
-
+            ////int[] exampleTest1 = { 2, 6, 8, -10, 3 };
+            ////Find(exampleTest1);
+            //Console.WriteLine(DuplicateEncode("Success"));
+            //string prueba = "cocacolao";
+            //int test = prueba.Split("o").Length;
+            //Console.WriteLine(test);
+            ////foreach (var letra in trozo)
+            ////{
+            ////    Console.WriteLine(letra);
+            ////}
+            
         }
         /*
          * [2, 4, 0, 100, 4, 11, 2602, 36]
@@ -77,6 +85,49 @@ namespace Curso
             }
             return newDna;
              
+        }
+
+        /*
+         * convert a string to a new string where each character 
+         * in the new string is "(" if that character appears only once in the original string, or ")" 
+         * if that character appears more than once in the original string. 
+         * Ignore capitalization when determining if a character is a duplicate.
+         * Examples:
+
+            "din"      =>  "((("
+            "recede"   =>  "()()()"
+            "Success"  =>  ")())())"
+            "(( @"     =>  "))((" 
+         */
+        public static string DuplicateEncode(string word)
+        {
+            word=word.ToLower();
+            //create a Dictionary to storage the times a char is repeated
+            Dictionary<char, int> charsInString = new Dictionary<char, int>();
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (!charsInString.ContainsKey(word[i]))                
+                    charsInString.Add(word[i],1);                    
+                
+                if(charsInString[word[i]] ==1)
+                {
+                    for (int b = 0; b < word.Length; b++)
+                    {
+                        if (i != b && word[i] == word[b])//conditional to skip the add of current letter
+                            charsInString[word[i]]++;         
+                    }
+                }
+                
+            }            
+            string finalString = "";
+            foreach (char valuestring in word)
+            {
+                if (charsInString[valuestring] > 1)
+                    finalString += ")";
+                else
+                    finalString += "(";
+            }
+            return finalString;
         }
     }
 }
